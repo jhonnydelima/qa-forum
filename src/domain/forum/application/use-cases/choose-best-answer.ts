135e726e87ed46cd868ffa8372ce4e0c062b21bc
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-19 20:27:33 -0300

    feat: add `choose-best-answer` use case

---
+++ src/domain/forum/application/use-cases/choose-best-answer.ts
@@ -0,0 +1,43 @@
+import { Question } from '../../enterprise/entities/question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+type ChooseBestAnswerUseCaseRequest = {
+  authorId: string
+  answerId: string
+}
+
+type ChooseBestAnswerUseCaseResponse = {
+  question: Question
+}
+
+export class ChooseBestAnswerUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private answersRepository: AnswersRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+  }: ChooseBestAnswerUseCaseRequest): Promise<ChooseBestAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+    question.bestAnswerId = answer.id
+    await this.questionsRepository.save(question)
+    return {
+      question,
+    }
+  }
+}
