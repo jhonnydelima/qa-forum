8ef329e604a0942211c3a44f9fefc4620bee271c
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-20 01:00:17 -0300

    feat: add `fetch-recent-questions` use case

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,23 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+type FetchRecentQuestionsUseCaseRequest = {
+  page: number
+}
+
+type FetchRecentQuestionsUseCaseResponse = {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionsRepository.findManyRecent({ page })
+    return {
+      questions,
+    }
+  }
+}
