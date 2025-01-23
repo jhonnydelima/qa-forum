b7478ed60f927ef2030f4516bd8643b5e159c73c
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-22 21:33:09 -0300

    feat: add `fetch-answer-comments` use case

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -0,0 +1,28 @@
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+type FetchAnswerCommentsUseCaseRequest = {
+  answerId: string
+  page: number
+}
+
+type FetchAnswerCommentsUseCaseResponse = {
+  answerComments: AnswerComment[]
+}
+
+export class FetchAnswerCommentsUseCase {
+  constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    answerId,
+    page,
+  }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
+    const answerComments =
+      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
+        page,
+      })
+    return {
+      answerComments,
+    }
+  }
+}
