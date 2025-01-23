02c43f55f297fcc0e39d5bda7c233001b71c6f70
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-22 21:24:43 -0300

    feat: add `fetch-question-comments` use case

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,28 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+type FetchQuestionCommentsUseCaseRequest = {
+  questionId: string
+  page: number
+}
+
+type FetchQuestionCommentsUseCaseResponse = {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments =
+      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+    return {
+      questionComments,
+    }
+  }
+}
