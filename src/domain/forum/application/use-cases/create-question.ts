3253f5311c2a9462e3755606fb31cc20c977903d
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-25 16:16:29 -0300

    refactor: update `question`, `create-question`, and `edit-question`

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -3,6 +3,7 @@ import { Question } from '../../enterprise/entities/question'
 import { UniqueIdEntity } from '@/core/entities/unique-id-entity'
 import { Either, right } from '@/core/either'
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
+import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list'
 
 type CreateQuestionUseCaseRequest = {
   authorId: string
@@ -38,7 +39,7 @@ export class CreateQuestionUseCase {
         questionId: question.id,
       })
     })
-    question.attachments = questionAttachments
+    question.attachments = new QuestionAttachmentList(questionAttachments)
     await this.questionsRepository.create(question)
     return right({
       question,
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,7 +3,7 @@ import { Slug } from './value-objects/slug'
 import { UniqueIdEntity } from '@/core/entities/unique-id-entity'
 import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
-import { QuestionAttachment } from './question-attachment'
+import { QuestionAttachmentList } from './question-attachment-list'
 
 export type QuestionProps = {
   authorId: UniqueIdEntity
@@ -11,7 +11,7 @@ export type QuestionProps = {
   title: string
   slug: Slug
   content: string
-  attachments: QuestionAttachment[]
+  attachments: QuestionAttachmentList
   createdAt: Date
   updatedAt?: Date
 }
@@ -77,7 +77,7 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.touch()
   }
 
-  set attachments(value: QuestionAttachment[]) {
+  set attachments(value: QuestionAttachmentList) {
     this.props.attachments = value
   }
 
@@ -89,7 +89,7 @@ export class Question extends AggregateRoot<QuestionProps> {
       {
         ...props,
         slug: props.slug ?? Slug.createFromText(props.title),
-        attachments: props.attachments ?? [],
+        attachments: props.attachments ?? new QuestionAttachmentList(),
         createdAt: props.createdAt ?? new Date(),
       },
       id,
